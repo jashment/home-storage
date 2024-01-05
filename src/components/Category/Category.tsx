eaa1fff1949c3f0053c0dc42365ace33a8f07eb4
Jordan <jordanashment@gmail.com>
2024-01-05 13:47:13 -0700

    Add some styling

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -6,17 +6,31 @@ interface CategoryTypes {
 
 const Category = ({ categoryName }: CategoryTypes) => {
     return (
-        <Box>{categoryName}</Box>
+        <Box>
+            <TWCategoryName>
+                {categoryName}
+            </TWCategoryName>
+        </Box>
     )
 }
 
 const Box = tw.div`
-  border-solid
-  border-2
-  border-black
-  h-20
-  w-20
-  m-3
+    border-solid
+    border-2
+    border-cyan-200
+    drop-shadow-md
+    rounded-md
+    bg-white
+    p-10
+    m-3
+`
+
+const TWCategoryName = tw.p`
+    flex
+    items-center
+    justify-center
+    my-auto
+    h-full
 `
 
 export default Category
