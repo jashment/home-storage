57ea63a9d05f7c5ee808688623fa5d403ddd6215
Jordan <jordanashment@gmail.com>
2024-01-08 13:36:06 -0700

    Adjusting sidebar/bottom nav

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -9,17 +9,31 @@ const Drawer = () => {
 }
 
 const TWDrawer = tw.div`
+    flex
     border-solid
-    border-2
-    h-full
+    border-black
+    border-t-2
+    md:border-t-0
+    bottom-0
+    w-full
     fixed
-    w-20
+    h-20
+    items-center
+    justify-between
+    md:border-r-2
+    md:h-full
+    md:w-20
+    md:items-start
 `
 
 const TWDrawerButton = tw.button`
     border-solid
     border-2
     drop-shadow-md
+    fixed
+    justify-center
+    invisible
+    md:visible
 `
 
 export default Drawer
