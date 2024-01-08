69b8dc2e0bfd2bdbc7e6cba1a77d0c97fb4d9074
Jordan <jordanashment@gmail.com>
2024-01-08 14:31:47 -0700

    Add icon pack

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -3,11 +3,25 @@ import tw from "tailwind-styled-components"
 const Drawer = () => {
     return (
         <TWDrawer>
-            <TWDrawerButton>Button</TWDrawerButton>
+            <TWDrawerButton>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+
+            </TWDrawerButton>
+
+
             <TWItem />
             <TWItem />
             <TWItem />
-            <TWItem />
+            <TWItem>
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                </button>
+            </TWItem>
+
         </TWDrawer>
     )
 }
